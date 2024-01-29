b812ddecb61c1fada458c2f6e86f08e53492033c
rdarlian <darlianramadhan@gmail.com>
2024-01-29 08:52:24 +0700

    template 1 update

--- resources/views/dashboard/template/1/index.blade.php
+++ resources/views/dashboard/template/1/index.blade.php
@@ -92,10 +92,10 @@
 </head>
 
 <body class="hidden">
-  <div class="cover-container " id="cover">
-    <div class="relative-box">
+  <div class="cover-container married-pop" id="cover">
+    <div class="display-flex flex-column align-center mlr-auto color-white">
       <img data-aos="zoom-in" data-aos-duration="600" data-aos-easing="ease-in-out" class="cover-img" src="{{ asset('/concept1/svg/oratas-1.svg') }}" alt="" />
-      <p data-aos="zoom-in" data-aos-duration="600" data-aos-easing="ease-in-out" class=" font-20 regular text-center pt-134">The Wedding of</p>
+      <p data-aos="zoom-in" data-aos-duration="600" data-aos-easing="ease-in-out" class="font-20 regular text-center pt-155">The Wedding of</p>
       <div data-aos="zoom-in" data-aos-duration="600" data-aos-easing="ease-in-out" class="w-275 mt-16 mlr-auto married-birth relative">
         <h1 class="font-60 regular lineletter">{{ $undangan->groom_nickname}}</h1>
         <h2 class="name-cover-1 text-center font-60 regular lineletter">&</h2>
@@ -108,7 +108,7 @@
         <div class="invitation-box text-center">
           <p class="dear">Dear</p>
           @if(request()->filled('r'))
-          <p class="name">{{ request()->r }}</p>
+          <p class="font-20 large">{{ request()->r }}</p>
           @endif
         </div>
         <p class="font-14 regular text-center">You are cordially invited to our wedding</p>
@@ -118,18 +118,17 @@
 
   </div>
   <div id="main" class="main-container married-pop bg-blue-1">
-    <div>
-      <audio autoplay loop>
+    <div class="sound">
+      <!-- <iframe src="{{ asset('/concept1i/silence.mp3') }}" allow="autoplay" id="audio"></iframe> -->
+      <audio id="player" autoplay loop>
         <source src="{{isset($songs[0]->audio_path) ? url('storage/' . $songs[0]->audio_path) : '' }}">
       </audio>
       <span class="suara" onclick="togglePlay()">
-        <img src="{{ asset('/concept1i/png/suara.png') }}" alt="">
+        <img id="suara-i" src="{{ asset('/concept1i/svg/suara.svg') }}" alt="">
       </span>
-
     </div>
 
     <div class="relative">
-
       <img data-aos="zoom-in" data-aos-duration="600" data-aos-easing="ease-in-out" class=" img-cover-1 wiggle" src="{{ asset('/concept1/svg/oratas-1.svg') }}" alt="" />
     </div>
     <div class="pt-220 pb-114 color-white">
@@ -137,13 +136,11 @@
       <div data-aos="zoom-in" data-aos-duration="600" data-aos-easing="ease-in-out" class="w-275 mt-16 mlr-auto married-birth relative">
         <h1 class="font-60 regular lineletter">{{ $undangan->groom_nickname}}</h1>
         <h2 class="name-cover-1 text-center font-60 regular lineletter">&</h2>
-        <br />
-        <br />
-        <h1 class="font-60 regular text-right lineletter">{{ $undangan->bride_nickname}}</h1>
+        <h1 class="font-60 mt-24 regular text-right lineletter">{{ $undangan->bride_nickname}}</h1>
       </div>
 
       <h2 class="font-18 mt-24 large text-center">{{$receptionDay}}, {{$receptionDate}}</h2>
-      <p class="font-14 regular text-center">{{$undangan->reception_location}}</p>
+      <p class="font-14 regular text-center">{{$undangan->alamatResepsi}}</p>
 
       <div class="text-center mt-81">
         <img src="{{ asset('/concept1/svg/arrow-up.svg') }}" alt="arrow-up" />
@@ -153,11 +150,11 @@
         <img class="img-cover-2 wiggle" src="{{ asset('/concept1/svg/orbawah-1.svg') }}" alt="" />
       </div>
     </div>
-    <div class=" pt-30 display-center flex-column color-white-rgb">
+    <div class=" py-60 display-center flex-column color-white-rgb">
       <img data-aos="fade-up" class="mlr-auto" src="{{ asset('/concept1/svg/ornament.svg') }}" alt="" />
 
-      <div class="w-327 mlr-auto text-center">
-        <h3 data-aos="fade-up" class="mt-16 font-18 medium">
+      <div class="w-327 mlr-auto text-center hero">
+        <h3 data-aos="fade-up" class="mt-20 font-18 medium">
           بِسْمِ اللّهِ الرَّحْمَنِ الرَّحِيْ
         </h3>
         <p data-aos="fade-up" class="font-14 regular mt-20">
@@ -170,14 +167,14 @@
         </p>
       </div>
 
-      <div class="pt-30 pb-99">
+      <div class="pb-99">
         <div class="position-relative w-100">
           <img class="position-absolute end-0" src="{{ asset('/concept1/png/Ornamentright1.png') }}" alt="">
         </div>
         <div class="w-307 mlr-auto">
-          <img data-aos="zoom-in" data-aos-duration="600" data-aos-easing="ease-in-out" class="img-mempelai" src="{{$groomImage[0]->images ?? ''}}" alt="" />
+          <img data-aos="zoom-in" data-aos-duration="600" data-aos-easing="ease-in-out" class="img-mempelai mt-40 mb-40" src="{{$groomImage[0]->images ?? ''}}" alt="" />
 
-          <div data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-out" class="name-item-mempelai text-center mt-36">
+          <div data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-out" class="name-item-mempelai text-center">
             <h1 class="font-28 medium married-birth">{{$undangan->groom_name}}</h1>
             <hr />
             <p class="font-12 regular">Putra {{ $undangan->groom_child_order }} dari</p>
@@ -201,13 +198,14 @@
       </div>
     </div>
 
-    <div class="pt-60 color-white">
+    <div class="color-white display-center flex-column align-center">
+      <img data-aos="fade-up" class="mlr-auto py-40" src="{{ asset('/concept1/svg/ornament.svg') }}" alt="" />
       <h1 data-aos="zoom-in" data-aos-duration="1000" data-aos-easing="ease-in-out" class="font-18 large text-center">Rangkaian Acara</h1>
       <div data-aos="zoom-in" data-aos-duration="1000" data-aos-easing="ease-in-out" class="img-ellipse-1">
         <img src="{{ asset('/concept1/svg/handshake.svg') }}" alt="" />
       </div>
       <img class="position-absolute start-0" src="{{ asset('/concept1/png/ornamentkiri1.png') }}" alt="">
-      <div class="w-327 mt-16 text-center mlr-auto">
+      <div class="w-327 mt-24 text-center mlr-auto">
         <h1 data-aos="zoom-in" data-aos-duration="1000" data-aos-easing="ease-in-out" class="font-18 large">Akad Nikah</h1>
         <div class="font-14 regular mt-16 flex flex-column gap-6">
           <p data-aos="zoom-in" data-aos-duration="1000" data-aos-easing="ease-in-out">{{$akadDay}}, {{$akadDate}} {{ date('H:i', strtotime($undangan->akad_time)) }} </p>
@@ -234,18 +232,18 @@
       </div>
       <!-- Story -->
       <div class="pt-105 display-center flex-column ">
-        <div class="display-center align-center flex-column mb-24">
-          <img class="position-absolute start-0" src="{{ asset('concept1/svg/orleft-2.svg') }}" alt="">
+        <div data-aos="zoom-in" data-aos-duration="600" data-aos-easing="ease-in-out" class="display-center align-center flex-column mb-24">
+          <img class="position-absolute orleft-2" src="{{ asset('concept1/svg/orleft-2.svg') }}" alt="">
           <img src="{{ asset('concept1/svg/storyheader.svg') }}" alt="">
           <p class="font-18 large mb-0 mt-24">Our Story Begin</p>
           <p class="font-14 regular">Cerita bagaimana kami dipertemukan</p>
         </div>
 
-        @foreach($stories as $story)
-        <div class="display-center flex-column align-center">
-          <img class="img-story" src="{{ url('storage/' . $story->image_story) ?? '' }}" alt="">
+        @foreach($stories as $key=>$story)
+        <div data-aos="zoom-in" data-aos-duration="600" data-aos-easing="ease-in-out" class="display-center flex-column align-center mt-32">
+          <img class="{{ isset($story->image_story) ? 'img-story ' : 'icon-story' }}" src="{{ isset($story->image_story) ? url('storage/' . $story->image_story) : asset('/concept1i/svg/story'. $key+1 .'.svg')  }}" alt="">
           <h1 class="font-18 large mt-16">{{ $story->title_story }}</h1>
-          <p class="w-327 font-14 regular text-center">
+          <p class="w-327 font-14 regular text-center mt-8">
             {{ $story->description_story }}
           </p>
         </div>
@@ -254,7 +252,7 @@
       <!-- Story end -->
 
       <!-- gallery -->
-      <div class="wrapper mt-97">
+      <div data-aos="zoom-in" data-aos-duration="600" data-aos-easing="ease-in-out" class="wrapper mt-97">
         <h1 class="font-18 large">Gallery</h1>
         <!-- <div class="boxes mt-31">
           <div class="slider">
@@ -288,7 +286,7 @@
       </div>
 
       <div class="message mt-120">
-        <div class="message-title color-white">
+        <div class="message-title color-white text-center">
           <h1 data-aos="zoom-in" data-aos-duration="600" data-aos-easing="ease-in-out" class="font-18 medium">Kirim Ucapan</h1>
           <p data-aos="zoom-in" data-aos-duration="600" data-aos-easing="ease-in-out" class="font-14 regular color-opacity">
             Rangkaian kata kata untuk kami yang Sedang berbagahia
@@ -347,7 +345,10 @@
         </div>
       </div>
 
-
+      <div class="relative w-100">
+        <img src="{{ asset('/concept1/svg/or-footer.svg') }}" alt="">
+        <img class="or-footerr" src="{{ asset('/concept1/svg/or-footerr.svg') }}" alt="">
+      </div>
     </div>
     <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog">
@@ -369,11 +370,11 @@
               </div>
 
               <div class="mb-3">
-                <!-- <select class="form-select my-4" name="konfirmasi" aria-label="Default select example">
-                <option selected value="4">Konfirmasi Kehadiran</option>
-                <option value="1">Hadir</option>
-                <option value="0">Tidak Hadir</option>
-              </select> -->
+                <select class="form-select my-4" name="konfirmasi" aria-label="Default select example">
+                  <option selected value="4">Konfirmasi Kehadiran</option>
+                  <option value="1">Hadir</option>
+                  <option value="0">Tidak Hadir</option>
+                </select>
                 <input data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease-in-out" type="hidden" name="slug" value="{{$undangan->slug}}">
                 <div>
                 </div>
@@ -387,17 +388,20 @@
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
 
     <script>
-      let a = 1;
+      let a = 0;
 
       function togglePlay() {
         if (a == 0) {
+          console.log('musiiic')
           document.getElementsByTagName("audio")[0].play();
           a++;
-          document.querySelector("i").style.opacity = "1";
+          $("#suara-i").attr('src', "{{ asset('/concept1i/svg/suara.svg') }}");
         } else {
           document.querySelector("audio").pause();
+
+          $("#suara-i").attr('src', "{{ asset('/concept1i/svg/suaraoff.svg') }}");
           a--;
-          document.querySelector("i").style.opacity = ".5";
+
         }
       };
     </script>
